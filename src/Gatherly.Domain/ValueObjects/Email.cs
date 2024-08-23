a18510128650fc352b00a702bdcfc775565e05bd
Giang <nguyenvietgiang1110@gmail.com>
2024-08-23 18:23:10 +0700

    Add project files.

---
+++ src/Gatherly.Domain/ValueObjects/Email.cs
@@ -0,0 +1,36 @@
+﻿using Gatherly.Domain.Errors;
+using Gatherly.Domain.Primitives;
+using Gatherly.Domain.Shared;
+
+namespace Gatherly.Domain.ValueObjects;
+
+public sealed class Email : ValueObject
+{
+    private Email(string value) => Value = value;
+
+    private Email()
+    {
+    }
+
+    public string Value { get; private set; }
+
+    public static Result<Email> Create(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return Result.Failure<Email>(DomainErrors.Email.Empty);
+        }
+
+        if (email.Split('@').Length != 2)
+        {
+            return Result.Failure<Email>(DomainErrors.Email.InvalidFormat);
+        }
+
+        return new Email(email);
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
---
+++ src/Gatherly.Persistence/Migrations/20240823110509_MigrationProject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gatherly.Persistence.Migrations
+{
+    public partial class MigrationProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
